5a87e5807a51d87d1e0dfaffa179d444693514e4
Cyclone Addons <90061864+CycloneAddons@users.noreply.github.com>
2025-10-25 17:40:06 +0530

    Update _buttons.py

--- src/cogs/esports/views/ssmod/_buttons.py
+++ src/cogs/esports/views/ssmod/_buttons.py
@@ -91,7 +91,7 @@ class ScreenshotType(discord.ui.Button):
 
             if not self.view.record.ss_type == SSType.yt:
                 if not await self.ctx.is_premium_guild():
-                    return await self.ctx.error("You need Quotient Prime to set this filter. (Use `qperks`)", 4)
+                    return await self.ctx.error("You need Quotient Legacy to set this filter. (Use `qperks`)", 4)
 
             if _v.custom_id == "custom":
                 _m = await self.ctx.simple(
